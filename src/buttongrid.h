92f51de13e11d89633eaa19d8a034c21e7d08229
Dan Mills <dmills@exponent.myzen.co.uk>
2011-07-14 23:35:37 +0100

    Formatting cleanup and removed some unused functions.

--- src/buttongrid.h
+++ src/buttongrid.h
@@ -33,25 +33,20 @@ class ButtonGrid : public QWidget
 {
     Q_OBJECT
 public:
-		/// Construct an empty ButtonGrid
+    /// Construct an empty ButtonGrid
 
-		///
     /// @param num_x is the number of buttons to create across the screen.
     /// @param num_y is the number of rows of buttons to create.
-		/// @param offset is the engine framesource index of the upper left corner
+    /// @param offset is the engine framesource index of the upper left corner
     /// @param *parent is the parent widget.
     ButtonGrid (EnginePtr engine_, unsigned int num_x = 8, unsigned int num_y = 8,unsigned int offset=0 ,QWidget *parent = NULL);
     ~ButtonGrid();
     /// @returns Returns a pointer to the ScreenDisplay corresponding to the given coordinates in the grid.
-
-		///
-		/// @param x the (zero based) X coordinate of the ScreenDisplay to be returned.
+    /// @param x the (zero based) X coordinate of the ScreenDisplay to be returned.
     /// @param y the (zero based) y coordinate of the ScreenDisplay to be returned.
     ScreenDisplay * at (unsigned int x, unsigned int y);
     /// Get the dimensions of the grid.
-
-		///
-		/// @param &x reference to variable to contain the x value.
+    /// @param &x reference to variable to contain the x value.
     /// @param &y reference to variable to contain the y value.
     void dimensions(int &x, int &y);
 
@@ -59,26 +54,24 @@ signals:
     /// Emitted when one of the buttons is modified.
     void modified ();
 public slots:
-		/// This gets called by the engine whenever a framesource has been replaced by a new one
-		/// Used to update the displayed grid
-		void frameSourceChanged(unsigned long int pos);
-		/// called when the screen display menu item import is selected
-		void message (QString text, int time);
+    /// This gets called by the engine whenever a framesource has been replaced by a new one
+    /// Used to update the displayed grid
+    void frameSourceChanged(unsigned long int pos);
+    /// called when the screen display menu item import is selected
+    void message (QString text, int time);
 
 private:
     QGridLayout * layout;
-		QSignalMapper * mapper;
-		QStatusBar *statusbar;
-		QLabel * ppsLabel;
-		QLabel * framesLabel;
+    QSignalMapper * mapper;
+    QStatusBar *statusbar;
+    QLabel * ppsLabel;
+    QLabel * framesLabel;
     unsigned int num_x_;
     unsigned int num_y_;
-		unsigned int offset_;
-		EnginePtr engine;
+    unsigned int offset_;
+    EnginePtr engine;
 private slots:
-    //void mod ();
-		void selectionChangedData(unsigned int pos, QColor col);
-		//void stateChanged (int);
+    void selectionChangedData(unsigned int pos, QColor col);
 };
 
 #endif
