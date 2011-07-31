351ea31ab23fdf8bb5c5c8fe3ef1b5539ad23eb9
Dan Mills <dmills@exponent.myzen.co.uk>
2011-07-31 01:08:59 +0100

    Added a method to enumerate all midi ports on the system.

--- src/alsamidi.h
+++ src/alsamidi.h
@@ -1,3 +1,21 @@
+/* alsamidi.h is part of lucifer a laser show controller.
+
+Copyrignt 2010 Dan Mills <dmills@exponent.myzen.co.uk>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 dated June, 1991.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #ifndef ALSAMIDI_INC
 #define ALSAMIDI_INC
 
@@ -18,7 +36,7 @@ public:
   qint64 readData (char *data, qint64 maxsize);
   qint64 writeData(const char *data, qint64 maxsize);
   bool isSequential () const;
-  std::vector<std::pair<QString,QString> > enumeratePorts() const;
+  static std::vector<std::pair<QString,QString> > enumeratePorts();
 private:
   snd_rawmidi_t *input;
   snd_rawmidi_t *output;
