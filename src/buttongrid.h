718eab03fab96d0391b5f7f15505548dda030689
Dan Mills <dmills@exponent.myzen.co.uk>
2011-12-11 18:28:04 +0000

    Documentation improvements.

--- src/buttongrid.h
+++ src/buttongrid.h
@@ -24,42 +24,43 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "screendisplay.h"
 #include "engine.h"
 
-/// A grid of ScreenDiplays displayed on the screen.
+/// \brief A grid of ScreenDiplays displayed on the screen.
 
 /// This diaplays a grid of ScreenDisplay objects inside whatever parent QWidget is used.
 /// Various slection modes are supported
-
 class ButtonGrid : public QWidget
 {
     Q_OBJECT
 public:
-    /// Construct an empty ButtonGrid
-
-    /// @param num_x is the number of buttons to create across the screen.
-    /// @param num_y is the number of rows of buttons to create.
-    /// @param offset is the engine framesource index of the upper left corner
-    /// @param *parent is the parent widget.
+    /// \brief Construct an empty ButtonGrid
+    /// @param [in] num_x is the number of buttons to create across the screen.
+    /// @param [in] num_y is the number of rows of buttons to create.
+    /// @param [in] offset is the engine framesource index of the upper left corner
+    /// @param [in] *parent is the parent widget (normal QT lifetime management applies).
     ButtonGrid (EnginePtr engine_, unsigned int num_x = 8, unsigned int num_y = 8,unsigned int offset=0 ,QWidget *parent = NULL);
     ~ButtonGrid();
-    /// @returns Returns a pointer to the ScreenDisplay corresponding to the given coordinates in the grid.
-    /// @param x the (zero based) X coordinate of the ScreenDisplay to be returned.
-    /// @param y the (zero based) y coordinate of the ScreenDisplay to be returned.
+    /// \brief obtain a pointer to the screendisplay at a given coordinate pair.
+    /// @returns Returns a pointer to the ScreenDisplay.
+    /// @param [in] x the (zero based) X coordinate of the ScreenDisplay to be returned.
+    /// @param [in] y the (zero based) y coordinate of the ScreenDisplay to be returned.
     ScreenDisplay * at (unsigned int x, unsigned int y);
-    /// Get the dimensions of the grid.
-    /// @param &x reference to variable to contain the x value.
-    /// @param &y reference to variable to contain the y value.
+    /// \brief Get the dimensions of the grid.
+    /// @param [out] &x reference to variable to contain the x value.
+    /// @param [out] &y reference to variable to contain the y value.
     void dimensions(int &x, int &y);
 
 signals:
-    /// Emitted when one of the buttons is modified.
+    /// \brief Emitted when one of the buttons is modified.
     void modified ();
 public slots:
-    /// This gets called by the engine whenever a framesource has been replaced by a new one
-    /// Used to update the displayed grid
+    /// \brief This gets called by the engine whenever a framesource has been replaced by a new one.
+    /// Used to update the displayed grid.
+    /// @param [in] pos is the engine index of the replaced framesource.
     void frameSourceChanged(unsigned long int pos);
-    /// called when the screen display menu item import is selected
+    /// \brief called to display a message in the status bar for the button widget.
+    /// @param [in] text is the text to display.
+    /// @param [in] time is the time in ms for which the text should be displayed.
     void message (QString text, int time);
-
 private:
     QGridLayout * layout;
     QSignalMapper * mapper;
